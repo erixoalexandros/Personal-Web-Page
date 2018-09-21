d8dc69f3aaa76b3f7245b3f82e7a131ab7e71ef6
Erick Jordan <erixoalexandros@gmail.com>
2018-09-21 16:58:02 -0500

    Added responsiveness until 1200px wide

--- js/index.js
+++ js/index.js
@@ -1,24 +1,41 @@
+//Load Page
 window.addEventListener('load', () => {
 
+  //DOM LOADER/////////////////////////////////////////////
+  const wrapper = document.getElementById('wrapper');
   const nav = document.getElementsByTagName('nav')[0];
-  const navListItems = nav.firstElementChild.children;
+  const navListItems = nav.firstElementChild.children;//Navigation bar items
   const about = document.getElementById('about');
   const skills = document.getElementById('skills');
   const experience = document.getElementById('experience');
   const education = document.getElementById('education');
-  const contact = document.getElementById('contact');
+  //////////////////////////////////////////////////////////
 
-  this.addEventListener('scroll', () => {
+  this.addEventListener('scroll', () => {//When scroll
 
-    if (window.scrollY > 100) {
-      if (!nav.classList.contains('nav-fixed')) {
+    if (window.scrollY > 100) {//When nav bar is not visible at the top
+
+      // Add Right Arrow if screen -1500px wide and doesn't exist
+      if (screen.width <= 1500 && wrapper.lastElementChild.id !== 'right-arrow') {
+        wrapper.insertAdjacentHTML('beforeend', '<img id="right-arrow" src="img/right-arrow.svg" alt="Right-Arrow">');
+      }
+      
+      if (!nav.classList.contains('nav-fixed')) {//Fix the nav bar to the right
         nav.classList.add('nav-fixed');
       }
+
     } else {
-      if (nav.classList.contains('nav-fixed')) {
+      
+      if (wrapper.lastElementChild.id === 'right-arrow') {//Remove Right Arrow Image
+        wrapper.removeChild(wrapper.lastElementChild);
+      }
+
+      if (nav.classList.contains('nav-fixed')) {//Unfix nav bar
         nav.classList.remove('nav-fixed');
       }
     }
+
+
 
     if (window.scrollY >= (about.offsetTop) && window.scrollY < (skills.offsetTop - 200)) {
 
