eb10a9c97fdf6c9c714afba3bf37359fa8fbdc44
Erick Jordan <erixoalexandros@gmail.com>
2018-09-24 16:20:59 -0500

    Responsiveness revised until 900px wide

--- js/index.js
+++ js/index.js
@@ -1,76 +1,47 @@
-//Load Page
-window.addEventListener('load', () => {
+'use strict';
+
+window.addEventListener('load', function () {//Load Page first
 
   //DOM LOADER/////////////////////////////////////////////
-  const wrapper = document.getElementById('wrapper');
-  const nav = document.getElementsByTagName('nav')[0];
-  const navListItems = nav.firstElementChild.children;//Navigation bar items
-  const about = document.getElementById('about');
-  const skills = document.getElementById('skills');
-  const experience = document.getElementById('experience');
-  const education = document.getElementById('education');
-  //////////////////////////////////////////////////////////
+  var wrapper = document.getElementById('wrapper');
+  var nav = document.getElementsByTagName('nav')[0];
+  //DOM LOADER/////////////////////////////////////////////
 
-  this.addEventListener('scroll', () => {//When scroll
+  this.addEventListener('scroll', function () {//When scroll
 
-    if (scrollY > 100) {//When nav bar is not visible at the top
+    navBarBehavior();
 
-      // Add Right Arrow if screen -1500px wide and doesn't exist
+  });
+
+  function navBarBehavior() {
+
+    if (pageYOffset > 100) {//When nav bar is not visible at the top
+
+      // Add Right Arrow if screen -1500px wide
       if (innerWidth <= 1500 && wrapper.lastElementChild.id !== 'right-arrow') {
-        console.log();
+
         wrapper.insertAdjacentHTML('beforeend', '<img id="right-arrow" src="img/right-arrow.svg" alt="Right Arrow">');
+
       }
-      
+
       if (!nav.classList.contains('nav-fixed')) {//Fix the nav bar to the right
+
         nav.classList.add('nav-fixed');
+
       }
 
     } else {
-      
+
       if (wrapper.lastElementChild.id === 'right-arrow') {//Remove Right Arrow Image
+
         wrapper.removeChild(wrapper.lastElementChild);
       }
 
       if (nav.classList.contains('nav-fixed')) {//Unfix nav bar
+
         nav.classList.remove('nav-fixed');
+        
       }
     }
-
-
-
-    if (window.scrollY >= (about.offsetTop) && window.scrollY < (skills.offsetTop - 200)) {
-
-      if (!navListItems[0].classList.contains('current-section')) {
-        navListItems[0].classList.add('current-section');
-      }
-    } else {
-      if (navListItems[0].classList.contains('current-section')) {
-        navListItems[0].classList.remove('current-section');
-      }
-    }
-
-    if (window.scrollY >= (skills.offsetTop - 200) && window.scrollY < (experience.offsetTop)) {
-
-      if (!navListItems[1].classList.contains('current-section')) {
-        navListItems[1].classList.add('current-section');
-      }
-    } else {
-      if (navListItems[1].classList.contains('current-section')) {
-        navListItems[1].classList.remove('current-section');
-      }
-    }
-
-    if (window.scrollY >= (experience.offsetTop) && window.scrollY < (education.offsetTop)) {
-
-      if (!navListItems[2].classList.contains('current-section')) {
-        navListItems[2].classList.add('current-section');
-      }
-    } else {
-      if (navListItems[2].classList.contains('current-section')) {
-        navListItems[2].classList.remove('current-section');
-      }
-    }
-
-  });
-
+  }
 });
