201f96f1d1068ecb3e1043a365730ee53a86ce29
Erick Jordan <erixoalexandros@gmail.com>
2018-09-25 15:47:37 -0500

    Nav Bar modified

--- js/index.js
+++ js/index.js
@@ -17,32 +17,16 @@ window.addEventListener('load', function () {//Load Page first
 
     if (pageYOffset > 100) {//When nav bar is not visible at the top
 
-      // Add Right Arrow if screen -1500px wide
-      if (innerWidth <= 1500 && !wrapper.lastElementChild.classList.contains('right-arrow')) {
-
-        wrapper.insertAdjacentHTML('beforeend', '<i class="fas fa-bars right-arrow"></i>');
-
-      }
-
       if (!nav.classList.contains('nav-fixed')) {//Fix the nav bar to the right
-
         nav.classList.add('nav-fixed');
-
       }
 
     } else {
 
-      if (wrapper.lastElementChild.classList.contains('right-arrow')) {//Remove Right Arrow Image
-
-        wrapper.removeChild(wrapper.lastElementChild);
-        
-      }
-
       if (nav.classList.contains('nav-fixed')) {//Unfix nav bar
-
-        nav.classList.remove('nav-fixed');
-        
+        nav.classList.remove('nav-fixed');   
       }
+      
     }
   }
 
