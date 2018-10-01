d3809c0967cba2876f91ee36c68fe494b14c094d
Erick Jordan <erixoalexandros@gmail.com>
2018-10-01 16:21:43 -0500

    Behavior added to the nav bar

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@
 window.addEventListener('load', function () {//Load Page first
 
   //DOM LOADER/////////////////////////////////////////////
-  var wrapper = document.getElementById('wrapper');
   var nav = document.getElementsByTagName('nav')[0];
+  var navItems = Array.prototype.slice.call(nav.firstElementChild.children);
+  var navBarExpander = document.getElementById('nav-menu-icon');
   //DOM LOADER/////////////////////////////////////////////
 
   this.addEventListener('scroll', function () {//When scroll
@@ -15,7 +16,7 @@ window.addEventListener('load', function () {//Load Page first
 
   function navBarBehavior() {
 
-    if (pageYOffset > 100) {//When nav bar is not visible at the top
+    if (pageYOffset > 100 && innerWidth > 750) {//When nav bar is not visible at the top
 
       if (!nav.classList.contains('nav-fixed')) {//Fix the nav bar to the right
         nav.classList.add('nav-fixed');
@@ -29,5 +30,24 @@ window.addEventListener('load', function () {//Load Page first
       
     }
   }
+
+  navBarExpander.addEventListener('click', function() {
+
+    if (nav.style.left !== '80%') {
+      nav.style.left = '80%';
+      this.style.display = "none";
+    }
+    
+  });
+
+  navItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      nav.style.left = '100%';
+      navBarExpander.style.display = 'block';
+    });
+  });
+    
+
+
 
 });
