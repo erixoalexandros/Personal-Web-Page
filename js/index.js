736f9431d83c2515044c7e6c17b799da6b15fb2c
Erick Jordan <erixoalexandros>
2018-10-01 22:00:28 -0500

    Header modified

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ window.addEventListener('load', function () {//Load Page first
   //DOM LOADER/////////////////////////////////////////////
   var nav = document.getElementsByTagName('nav')[0];
   var navItems = Array.prototype.slice.call(nav.firstElementChild.children);
-  var navBarExpander = document.getElementById('nav-menu-icon');
+  var navIcon = document.getElementById('nav-menu-icon');
+  var navCloseIcon = document.getElementById('nav-close-icon');
   //DOM LOADER/////////////////////////////////////////////
 
   this.addEventListener('scroll', function () {//When scroll
 
     navBarBehavior();
-
   });
 
   function navBarBehavior() {
@@ -26,28 +26,28 @@ window.addEventListener('load', function () {//Load Page first
 
       if (nav.classList.contains('nav-fixed')) {//Unfix nav bar
         nav.classList.remove('nav-fixed');   
-      }
-      
+      }      
     }
   }
 
-  navBarExpander.addEventListener('click', function() {
+  navIcon.addEventListener('click', function() {
 
-    if (nav.style.left !== '80%') {
       nav.style.left = '80%';
-      this.style.display = "none";
-    }
-    
+      this.style.left = '100%';
+      this.style.opacity = '0';
+      navCloseIcon.style.display = 'block';
   });
 
   navItems.forEach(function(item) {
+
     item.addEventListener('click', function() {
-      nav.style.left = '100%';
-      navBarExpander.style.display = 'block';
+
+      if (innerWidth <= 750) {
+
+        nav.style.left = '100%';
+        navIcon.style.left = 'calc(95% - 62px)';
+        navIcon.style.opacity = '1';
+      }
     });
   });
-    
-
-
-
 });
