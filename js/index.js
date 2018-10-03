a8bea4003ed8129b28b7a458b574bbeb977a9db1
Erick Jordan <erixoalexandros>
2018-10-02 21:39:35 -0500

    Some responsive behaviors fixed

--- js/index.js
+++ js/index.js
@@ -4,16 +4,11 @@ window.addEventListener('load', function () {//Load Page first
 
   //DOM LOADER/////////////////////////////////////////////
   var nav = document.getElementsByTagName('nav')[0];
-  var navItems = Array.prototype.slice.call(nav.firstElementChild.children);
-  var navIcon = document.getElementById('nav-menu-icon');
-  var navCloseIcon = document.getElementById('nav-close-icon');
+  // var navItems = Array.prototype.slice.call(nav.firstElementChild.children);
+  // var navIcon = document.getElementById('nav-menu-icon');
+  // var navCloseIcon = document.getElementById('nav-close-icon');
   var aboutPicture = document.querySelector('#about img');
   //DOM LOADER/////////////////////////////////////////////
-
-  this.addEventListener('scroll', function () {//When scroll
-
-    navBarBehavior();
-  });
 
   function navBarBehavior() {
 
@@ -33,33 +28,61 @@ window.addEventListener('load', function () {//Load Page first
 
   }
 
-  navIcon.addEventListener('click', function() {
+  function pictureOrientation () {
 
-      nav.style.left = '80%';
-      this.style.left = '100%';
-      this.style.opacity = '0';
-      navCloseIcon.style.display = 'block';
+    if (innerWidth < 750) {
+      aboutPicture.src = 'img/me-landscape.png';
+    } else {
+      aboutPicture.src = 'img/me-portrait.png';
+    }
 
-  });
-
-  navItems.forEach(function(item) {
-
-    item.addEventListener('click', function() {
-
-      if (innerWidth <= 750) {
-        nav.style.left = '100%';
-        navIcon.style.left = '80%';
-        navIcon.style.opacity = '1';
-      }
-
-    });
-
-  });
-
-  if (innerWidth < 750) {
-    aboutPicture.src = 'img/me-landscape.png';
-  } else {
-    aboutPicture.src = 'img/me-portrait.png';
   }
+
+  navBarBehavior();
+  pictureOrientation();
+
+  this.addEventListener('scroll', function () {//When scroll
+
+    navBarBehavior();
+
+  });
+
+  this.addEventListener('resize', function () {
+    
+    navBarBehavior();
+    pictureOrientation();
+    nav.style.transition = 'none';
+
+    if (innerWidth <= 900) {
+      nav.style.transition = 'none';   
+    } else {
+      nav.style.transition = 'left 0.5s';
+    }
+
+  });
+
+  // navIcon.addEventListener('click', function() {
+
+  //     nav.style.left = '80%';
+  //     this.style.left = '100%';
+  //     this.style.opacity = '0';
+  //     navCloseIcon.style.display = 'block';
+
+  // });
+
+  // navItems.forEach(function(item) {
+
+  //   item.addEventListener('click', function() {
+
+  //     if (innerWidth <= 750) {
+  //       nav.style.left = '100%';
+  //       navIcon.style.left = 'calc(90% - 2rem)';
+  //       navIcon.style.opacity = '1';
+  //       navCloseIcon.style.display = 'none';
+  //     }
+
+  //   });
+
+  // });
 
 });
